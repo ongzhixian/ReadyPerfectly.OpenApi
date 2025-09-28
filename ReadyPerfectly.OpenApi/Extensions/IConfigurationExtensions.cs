bd83f9b374ccf1ced12e01289adca1c858d1417b
ongzhixian <zhixian@hotmail.com>
2025-09-28 09:51:02 +0800

    Initial commit

---
+++ ReadyPerfectly.OpenApi/Extensions/IConfigurationExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.OpenApi.Models;
+
+namespace ReadyPerfectly.OpenApi.Extensions;
+
+public static class IConfigurationExtensions
+{
+    public static OpenApiInfo GetOpenApiInfoFromSection(
+        this IConfiguration configuration, 
+        string configurationSectionName)
+    {
+        var openApiInfo = new OpenApiInfo();
+        configuration.GetSection(configurationSectionName).Bind(openApiInfo);
+        return openApiInfo;
+    }
+}
